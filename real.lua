1758327ce752417f3821478d048e11d3bb857de3
MisterWaffes <190789764+MisterWafflesAndPancakes@users.noreply.github.com>
2025-12-05 21:16:48 +0000

    Update real.lua

--- real.lua
+++ real.lua
@@ -5,8 +5,8 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
 -- Settings
-local holdDuration = 3 -- how long to hold E
-local teleportDelay = 3 -- equal delay between teleports
+local holdDuration = 2.8 -- how long to hold E
+local teleportDelay = 4 -- equal delay between teleports
 local cycleDelay = 600 -- 10 minutes in seconds
 local running = false -- controlled by Rayfield toggle
 
@@ -16,7 +16,7 @@ local fsTrain = workspace.Main.TrainingAreasHitBoxes.FS["1No"]
 local psTrain = workspace.Main.TrainingAreasHitBoxes.PS["1No"]
 
 -- Default return area
-local returnArea = btTrain
+local returnArea = psTrain
 
 -- Function to simulate holding E
 local function holdE()
@@ -24,6 +24,14 @@ local function holdE()
     task.wait(holdDuration)
     VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.E, false, game)
 end
+
+-- Teleports to true centre
+local function returnToArea(area)
+    if area and area:IsA("BasePart") then
+        humanoidRootPart.CFrame = CFrame.new(area.Position)
+    end
+end
+
 
 -- Main loop
 task.spawn(function()
@@ -37,7 +45,7 @@ task.spawn(function()
             end
 
             -- Return to selected training area
-            humanoidRootPart.CFrame = returnArea.CFrame + Vector3.new(0, 5, 0)
+            returnToArea(returnArea)
 
             -- Wait until next cycle (10 minutes)
             task.wait(cycleDelay)
@@ -53,7 +61,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
     Name = "Auto presents",
     LoadingTitle = "Auto Presents",
-    LoadingSubtitle = "by MisterWaffles",
+    LoadingSubtitle = "by MisterWaffles :3",
 })
 
 local Tab = Window:CreateTab("Main", 4483362458)
@@ -70,7 +78,7 @@ Tab:CreateToggle({
 Tab:CreateDropdown({
     Name = "Return Area (1No)",
     Options = {"BT","FS","PS"},
-    CurrentOption = "BT",
+    CurrentOption = "PS",
     Flag = "ReturnArea",
     Callback = function(Option)
         if Option == "BT" then
